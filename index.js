a25b861970abd9d3a74cce8b61a85e0bf570c0be
Anurag Mishra <am91294@gmail.com>
2020-07-15 00:40:26 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var options = {year: 'numeric', month: 'long', day: 'numeric' };
 
 
 
@@ -10,20 +10,20 @@ return response.json();
 .then((data)=>{
     var opt = null;
     var sel = document.getElementById("countryList");
-    for(i = 0; i<data.length; i++) { 
+    for(i = 0; i<data.length; i++) {
         console.log(data[i].countryInfo.iso2);
         console.log(data[i].country);
         opt = document.createElement('option');
         opt.value = data[i].countryInfo.iso2;
         opt.innerHTML = data[i].country;
         sel.appendChild(opt);
-    }  
+    }
 
 })
 
-function getOption() { 
-    selectElement = document.querySelector('#countryList'); 
-    output = selectElement.value; 
+function getOption() {
+    selectElement = document.querySelector('#countryList');
+    output = selectElement.value;
 
     fetch ('https://corona.lmao.ninja/v2/countries/'+output)
     .then((response)=>{
@@ -32,7 +32,7 @@ function getOption() {
     })
     .then((data)=>{
     newDate = new Date(data.updated);
-    document.getElementById("timezone").innerHTML = newDate.toLocaleDateString("en-US",options);    
+    document.getElementById("timezone").innerHTML = newDate.toLocaleDateString("en-US",options);
     document.getElementById("flag").src = data.countryInfo.flag;
     document.getElementById("country").innerHTML = data.country;
     document.getElementById("active").innerHTML = data.active;
@@ -42,7 +42,7 @@ function getOption() {
     document.getElementById("tests").innerHTML = data.tests;
     document.getElementById("recovered").innerHTML = data.recovered;
     })
-} 
+}
 
 fetch ('https://corona.lmao.ninja/v2/countries/'+'in')
 .then((response)=>{
@@ -51,7 +51,7 @@ return response.json();
 })
 .then((data)=>{
 newDate = new Date(data.updated);
-document.getElementById("timezone").innerHTML = newDate.toLocaleDateString("en-US",options);   
+document.getElementById("timezone").innerHTML = newDate.toLocaleDateString("en-US",options);
 document.getElementById("flag").src = data.countryInfo.flag;
 document.getElementById("country").innerHTML = data.country;
 document.getElementById("active").innerHTML = data.active;
